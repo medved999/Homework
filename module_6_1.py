1e1b158dd781404bf61b8336ba5a742bd5eebeb2
Максим Медведев <reconnaissance1991@mail.ru>
2024-08-05 08:21:23 +0800

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,43 @@
+class Animal:
+    alive = True
+    fed = False
+    def eat(self, food):
+        self.food = food
+        if isinstance(food, Fruit):
+            print(f"{self.name} съел {food.name} ")
+            Animal.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name} ")
+            Animal.alive = False
+    def __init__(self, name):
+        self.name = name
+class Plant:
+    adible = False
+    def __init__(food, name):
+        food.name = name
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    edible = True
+
+
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
