77d0adbfe9449c86aa76798bd355d442a2ba93d1
Максим Медведев <reconnaissance1991@mail.ru>
2025-01-04 23:04:21 +0800

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,33 +1,30 @@
 import os
 import time
 
-#os.path.join
 path1 = r"C:"
 path2 = r"\papka1"
 path3 = "papka2"
-directory = os.path.join(path1, path2, path3 + r"\123")                   #собрали путь через join
+directory = os.path.join(path1, path2, path3 + r"\123")
 #print(directory)
 if not os.path.exists(directory):
-    os.makedirs(directory)                                                 #создали путь если отсутствует
-#os.walk
-a = os.walk(path2)
-for i in a:                                                  #пробегаемся через walk по вложенным папкам
-    print(i)                                                              #и выводим на экран
+    os.makedirs(directory)
 os.chdir(directory)
-name = "text.txt"                                              #создаем txt
-file = open(name, "a")
-file.close()
-directory1 = os.path.join(directory, name)
-
-time1 = os.path.getmtime(directory1)
-print(time1)
-print(time.ctime(time1))                                         #время создания файла
-
-print(os.path.getsize(directory1))                                #размер файла
+name = "text.txt"
 file = open(name, "a")
 file.write("123456789")
 file.close()
-print(os.path.getsize(directory1))                                 #размер файла после изменения
-
-print(os.path.dirname(directory1))
-
+for address, dirs, files in os.walk(directory):
+    for name in files:
+        filepath = os.path.join(address, name)
+        filetime = os.path.getmtime(name)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(name)
+        parent_dir = address
+        #print(filepath)
+        #print(time.ctime(filetime))
+        #print(files)
+        #print(formatted_time)
+        #print((filesize))
+        #print(parent_dir)
+print(f"Обнаружен файл: {name}, Путь: {filepath}, Размер: {filesize} байт, "
+      f"Время изменения: {formatted_time}, Родительская директория: {parent_dir}")
